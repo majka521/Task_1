b0188ca67467c962a453b19f1ede0c684829d9d0
majka521 <521majka@gmail.com>
2022-01-06 16:50:41 +0100

    header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,51 +1,54 @@
 export const Header = () => {
   return (
     <header className="header">
-      <div className="container">
-        <nav className="header__nav">
-          <ul className="header__nav__list">
-            <li className="header__nav__list__element">
-              <a href="." className="header__nav__list__element__link">
-                Buy
-              </a>
-            </li>
-            <li className="header__nav__list__element">
-              <a href="." className="header__nav__list__element__link">
-                Sell
-              </a>
-            </li>
-            <li className="header__nav__list__element">
-              <a href="." className="header__nav__list__element__link">
-                About
-              </a>
-            </li>
-          </ul>
-          <ul className="header__nav__panel">
-            <li className="header__nav__panel__element">
-              <a href="." className="header__nav__panel__element__register">
-                Register
-              </a>
-            </li>
-            <li className="header__nav__panel__element">
-              <a href="." className="header__nav__panel__element__login">
-                Login
-              </a>
-            </li>
-          </ul>
-        </nav>
-        <div className="header__home">
-          <ul className="header__home__list">
-            <li className="header__home__list__element">
-              <a href="." className="header__home__list__element__link">
-                Home
-              </a>
-            </li>
-            <li className="header__home__list__element">
-              <a href="." className="header__home__list__element__link">
-                Login
-              </a>
-            </li>
-          </ul>
+      <div className="header--wrapper__nav">
+        <div className="container">
+          <nav className="header__nav">
+            <ul className="header__nav__list">
+              <li className="header__nav__list__element">
+                <a href="#" className="header__nav__list__element__link">
+                  Buy
+                </a>
+              </li>
+              <li className="header__nav__list__element">
+                <a href="#" className="header__nav__list__element__link">
+                  Sell
+                </a>
+              </li>
+              <li className="header__nav__list__element">
+                <a href="#" className="header__nav__list__element__link">
+                  About
+                </a>
+              </li>
+            </ul>
+            <ul className="header__nav__panel">
+              <li className="header__nav__panel__element__register">
+                <a href="#">Register</a>
+              </li>
+              <li className="header__nav__panel__element__login">
+                <a href="#">Login</a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+
+      <div className="header--wrapper__home">
+        <div className="container">
+          <div className="header__home">
+            <ul className="header__home__list">
+              <li className="header__home__list__element">
+                <a href="#" className="header__home__list__element__home">
+                  Home
+                </a>
+              </li>
+              <li className="header__home__list__element">
+                <a href="#" className="header__home__list__element__login">
+                  / Login
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </header>
