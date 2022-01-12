6d48fce4968aded24d4e5cced952f6f74384403f
majka521 <521majka@gmail.com>
2022-01-12 21:39:15 +0100

    validation, read.me

---
+++ README.md
@@ -0,0 +1,25 @@
+# Recruitment task
+
+## ![Description icon by Icons8](https://img.icons8.com/dusk/24/000000/document--v1.png) Description
+
+You can see the page **[here](majka521.github.io/task_1/)**.
+
+## ![Tools icon by Icons8](https://img.icons8.com/external-icongeek26-linear-colour-icongeek26/24/000000/external-tools-plumbing-icongeek26-linear-colour-icongeek26.png) Technologies
+
+This project was created using the following technologies:
+
+- JavaScript
+- React.js with Hooks (useState, useEffect) and Create React App
+- SASS
+- NPM
+- WebPack
+
+![JavaScript icon by Icons8](https://img.icons8.com/color/48/000000/javascript--v2.png)
+![React icon by Icons8](https://img.icons8.com/color/48/000000/react-native.png)
+![Sass icon by Icons8](https://img.icons8.com/color/48/000000/sass.png)
+![NPM icon by Icons8](https://img.icons8.com/windows/48/000000/npm.png)
+![Webpack icon by Icons8](https://img.icons8.com/color/48/000000/webpack.png)
+
+---
+
+icons form [icons8](https://icons8.com/)
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,29 @@
+import React, { useState, useEffect } from "react";
+
 export const Main = () => {
+  //Login button validation
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [disabledLogIn, setDisabledLogIn] = useState(true);
+  const [nameOfClassLogIn, setNameOfClassLogIn] = useState("btn-inActive");
+
+  useEffect(() => {
+    const reg = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
+    if (username !== "" && username.length <= 100 && email !== "" && reg.test(email) && password.length >= 7 && password.length <= 25) {
+      setNameOfClassLogIn("");
+      setDisabledLogIn("");
+    } else {
+      setNameOfClassLogIn("btn-inActive");
+      setDisabledLogIn(true);
+    }
+  }, [username, email, password]);
+
+  const handleLogIn = (e) => {
+    e.preventDefault();
+    console.log("Log In");
+  };
+
   return (
     <main className="main">
       <div className="container">
@@ -9,14 +34,16 @@ export const Main = () => {
             <div className="main__box__login">
               <form className="main__box__login__form">
                 <label>Username</label>
-                <input></input>
+                <input type="text" placeholder={"john123"} name="Username" value={username} onChange={(e) => setUsername(e.target.value)}></input>
                 <label>Email Address</label>
-                <input></input>
+                <input type="email" placeholder={"john.doe@gmail.com"} name="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
                 <label>Password</label>
-                <input></input>
+                <input type="password" placeholder={"**********"} name="Password" value={password} onChange={(e) => setPassword(e.target.value)}></input>
                 <div className="main__box__login__form__submit">
                   <a href="#">Forgot Password?</a>
-                  <button className="btn">Login</button>
+                  <button disabled={disabledLogIn} onClick={handleLogIn} className={`${nameOfClassLogIn} btn`}>
+                    Login
+                  </button>
                 </div>
               </form>
             </div>
